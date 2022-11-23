16e7282c2b3dcd2cc73e21ef50ecb76683c238e3
Михаил Костров <mdkostrov@gmail.com>
2022-11-23 21:31:44 +0300

    v2

--- homework.py
+++ homework.py
@@ -10,7 +10,8 @@ from dotenv import load_dotenv
 from http import HTTPStatus
 
 from exceptions import (
-    AnswerError, DictionaryError, RequestError, UndocumentedStatusError
+    AnswerError, DictionaryError,
+    RequestError, UndocumentedStatusError
 )
 
 load_dotenv()
@@ -46,13 +47,15 @@ logger.addHandler(
 def send_message(bot, message):
     """Отправляет сообщение в Телеграм."""
     try:
+        logger.debug(
+            f'Попытка отправить сообщение в Telegram.')
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.debug(
             f'Сообщение в Telegram отправлено: {message}')
     except telegram.TelegramError as tg_error:
-        logger.error(
-            f'Сообщение в Telegram не отправлено: {tg_error}'
-        )
+        raise telegram.TelegramError(
+            f'Ошибка отправки сообщения в Telegram. {tg_error}'
+        ) from tg_error
 
 
 def get_api_answer(timestamp: int) -> dict:
@@ -66,7 +69,6 @@ def get_api_answer(timestamp: int) -> dict:
                 f'Эндпоинт {ENDPOINT} недоступен!'
                 f'HTTP-код ответа: {response.status_code}'
             )
-            logger.error(api_error)
             raise AnswerError(api_error)
         return response.json()
     except requests.exceptions.RequestException as request_error:
@@ -74,63 +76,56 @@ def get_api_answer(timestamp: int) -> dict:
             f'Ошибка запроса (RequestException).'
             f'Код ответа API: {request_error}'
         )
-        logger.error(api_error)
         raise RequestError(api_error) from request_error
     except json.JSONDecodeError as value_error:
         api_error = (
             f'Ошибка значения (ValueError). Код ответа API: {value_error}'
         )
-        logger.error(api_error)
         raise json.JSONDecodeError(api_error) from value_error
 
 
 def check_response(response: dict) -> list:
     """Проверяет полученные от API данные."""
-    if not type(response) is dict:
+    if not isinstance(response, dict):
         error_message = (
             'Полученные данные не являются словарем!'
         )
-        logger.error(error_message)
         raise TypeError(error_message)
-    if response.get('homeworks') is None:
+    homeworks = response.get('homeworks')
+    api_keys = list(response.keys())
+    if not 'homeworks' in api_keys or not 'current_date' in api_keys:
         error_message = (
-            'Ключ homeworks имеет неверное значение'
+            'Отсутствуют ключи homeworks или current_date '
+            'в полученном словаре'
         )
-        logger.error(error_message)
         raise DictionaryError(error_message)
-    if not type(response.get('homeworks')) is list:
+    if not isinstance(homeworks, list):
         error_message = (
             'Данные по ключу homeworks не являются списком!'
         )
-        logger.error(error_message)
         raise TypeError(error_message)
-    if response['homeworks'] == []:
-        return {}
-    return response['homeworks'][0]
+    return homeworks
 
 
 def parse_status(homework: dict) -> str:
     """Извлекает из ответа API статус домашней работы."""
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    if homework_name is None:
+    if not 'homework_name' in homework:
         error_message = (
-            f'Ошибка: пустое значение homework_name {homework_name}'
+            f'Ошибка: отсутствует ключ homework_name: {homework_name}'
         )
-        logger.error(error_message)
         raise UndocumentedStatusError(error_message)
-    if homework_status is None:
+    if not 'status' in homework:
         error_message = (
-            f'Ошибка: пустое значение homework_status {homework_status}'
+            f'Ошибка: отсутствует ключ status: {homework_status}'
         )
-        logger.error(error_message)
         raise UndocumentedStatusError(error_message)
-    if homework_status not in HOMEWORK_VERDICTS:
+    if not homework_status in HOMEWORK_VERDICTS:
         error_message = (
             f'Ошибка: незадокументированный статус'
-            f'домашней работы {homework_status}'
+            f'домашней работы: {homework_status}'
         )
-        logger.error(error_message)
         raise UndocumentedStatusError(error_message)
     verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
@@ -138,26 +133,7 @@ def parse_status(homework: dict) -> str:
 
 def check_tokens() -> bool:
     """Проверка присутствия переменных окружения (токенов)."""
-    tokens_error = (
-        'Отсутствует переменная окружения (токен):'
-    )
-    tokens_exist = True
-    if PRACTICUM_TOKEN is None:
-        tokens_exist = False
-        logger.critical(
-            f'{tokens_error} PRACTICUM_TOKEN'
-        )
-    if TELEGRAM_TOKEN is None:
-        tokens_exist = False
-        logger.critical(
-            f'{tokens_error} TELEGRAM_TOKEN'
-        )
-    if TELEGRAM_CHAT_ID is None:
-        tokens_exist = False
-        logger.critical(
-            f'{tokens_error} TELEGRAM_CHAT_ID'
-        )
-    return tokens_exist
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 
 def main():
@@ -173,9 +149,9 @@ def main():
     while True:
         try:
             response = get_api_answer(current_timestamp)
-            homework = check_response(response)
-            if homework:
-                message = parse_status(homework)
+            homeworks = check_response(response)
+            if homeworks:
+                message = parse_status(homeworks[0])
                 if current_status != message:
                     current_status = message
                     send_message(bot, message)
